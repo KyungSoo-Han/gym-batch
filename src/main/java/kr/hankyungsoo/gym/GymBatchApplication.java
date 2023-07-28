0564ac31899bcb6d317b9f31f015e7421851b643
kyungsoo han <hanks6160@gmail.com>
2023-07-28 22:51:03 +0900

    first commit

---
+++ src/main/java/kr/hankyungsoo/gym/GymBatchApplication.java
@@ -0,0 +1,13 @@
+package kr.hankyungsoo.gym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/hankyungsoo/gym/GymBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hankyungsoo.gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
